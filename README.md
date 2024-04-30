182b15ff1f8f95a937ce2d8e8d2ac72886299083
ajiang21 <127966003+ajiang21@users.noreply.github.com>
2024-04-30 15:12:01 -0400

    Fanuc-Arm GUI
    Matlab GUI of Fanuc LR Mate 200iC Arm

--- README.md
+++
@@ -1,3 +0,0 @@
-# RBE501-S24-S02
-
-Codebase and file repository for the course project submission for RBE 501, Robot Dynamics, in Spring 2024.
--- cal_IMat_c.m
+++
@@ -1,9 +0,0 @@
-function I_mat = cal_IMat_c( r,h, m )
-    Ixx = m* (3 * r^2 + h^2)/12;
-    Iyy = m* (3 * r^2 + h^2)/12;
-    Izz = m* r^2 /2; 
-
-    I_mat = [Ixx 0 0 ;
-            0 Iyy 0;
-            0 0 Izz];
-end
